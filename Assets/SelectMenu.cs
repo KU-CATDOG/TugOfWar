8ac2afb59a76463e56b33f4d29726c6c7d50b251
Hyunjin <hyunjin1109@korea.ac.kr>
2021-01-17 13:46:18 +0900

    SelectMenu UI

---
+++ Assets/SelectMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class SelectMenu : MonoBehaviour
+{
+    public Button temp;
+
+    public void button1()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button2()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button3()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button4()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button5()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button6()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button21()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button22()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button23()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button24()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button25()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    public void button26()
+    {
+        Debug.Log("버튼을 누른다");
+    }
+    
+}
