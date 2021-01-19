e0e5472aeb1344b681c407327b2af86912a58803
Hyunjin <hyunjin1109@korea.ac.kr>
2021-01-19 12:37:23 +0900

    Added Character class, student character

---
+++ Assets/ScriptsTemp/Character/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public float force { get; set; }
+    public int player { get; set; }  //1P: 0, 2P: 1
+    public int count { get; protected set; }
+
+    public bool freeze = false;
+
+    protected Animator animator;
+
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public float returnForce()
+    {
+        float temp = count * force;
+        count = 0;
+        return temp;
+    }
+
+}
---
+++ Assets/ScriptsTemp/Character/Student.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Student : Character
+{
+    protected override void Start()
+    {
+        base.Start();
+        force = 10;
+        player = 1; //temp
+    }
+
+    void Update()   //get pull key input
+    {
+        if (player == 0)    //Player1
+        {
+            if (Input.GetKeyDown("a") && !freeze){
+                count++;
+                Debug.Log(returnForce());
+            }
+        }
+        if (player == 1)    //Player2
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) && !freeze){
+                count++;
+                Debug.Log(returnForce());
+            }
+        }
+
+    }
+}
