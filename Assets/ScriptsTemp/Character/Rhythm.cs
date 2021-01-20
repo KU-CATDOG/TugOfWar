4eaaab543008b462ce0c1c7d07c6d039cec595f5
DESKTOP-RG00A65\Rozkhara <nytemute@gmail.com>
2021-01-20 17:10:20 +0900

    Edited Rhythm character to get key input only once within the time range

--- Assets/ScriptsTemp/Character/Rhythm.cs
+++ Assets/ScriptsTemp/Character/Rhythm.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rhythm : Character
 {
     float startTime;
+    int keyCount;
 
     protected override void Start()
     {
@@ -12,68 +13,77 @@ public class Rhythm : Character
         force = 10;
         player = 1; //temp
         startTime = Time.time;
+        keyCount = 0;
     }
     
     
 
-    void Update()   //get pull key input
+    void Update()   //run timer and get key input
     {
         float t = Time.time - startTime;
-        if (t >= 4)
+        if (t >= 2)
         {
             startTime = Time.time;
             t = 0;
+            keyCount = 0;
         }
 
         if (player == 0)    //Player1
         {
-            if ((Input.GetKeyDown("a") || Input.GetKeyDown("d")) && !freeze)
+            if(keyCount == 0)
             {
-                if (t < 0.25 || (t > 1.75 && t < 2.25) || t > 3.75)         //judge miss
+                if ((Input.GetKeyDown("a") || Input.GetKeyDown("d")) && !freeze)
                 {
-                    force = 0;
+                    if (t < 0.25 || t > 1.75)         //judge miss
+                    {
+                        force = 0;
+                    }
+                    else if ((t >= 0.25 && t < 0.5) || (t > 1.5 && t <= 1.75))       //judge good
+                    {
+                        force = 5;
+                    }
+                    else if ((t >= 0.5 && t < 0.75) || (t > 1.25 && t <= 1.5))       //judge great
+                    {
+                        force = 8;
+                    }
+                    else        //judge perfect
+                    {
+                        force = 12;
+                    }
+                    count++;
+                    keyCount++;
+                    Debug.Log(returnForce());
                 }
-                else if ((t >= 0.25 && t < 0.5) || (t > 1.5 && t <= 1.75) || (t >= 2.25 && t < 2.5) || (t > 3.5 && t <= 3.75))       //judge good
-                {
-                    force = 5;
-                }
-                else if ((t >= 0.5 && t < 0.75) || (t > 1.25 && t <= 1.5) || (t >= 2.5 && t < 2.75) || (t > 3.25 && t <= 3.5))       //judge great
-                {
-                    force = 8;
-                }
-                else        //judge perfect
-                {
-                    force = 12;
-                }
-                count++;
-                Debug.Log(returnForce());
             }
         }
 
         if (player == 1)    //Player2
         {
-            if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) && !freeze)
+            if (keyCount == 0)
             {
-                if (t < 0.25 || (t > 1.75 && t < 2.25) || t > 3.75)         //judge miss
+                if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) && !freeze)
                 {
-                    force = 0;
+                    if (t < 0.25 || (t > 1.75 && t < 2.25) || t > 3.75)         //judge miss
+                    {
+                        force = 0;
+                    }
+                    else if ((t >= 0.25 && t < 0.5) || (t > 1.5 && t <= 1.75) || (t >= 2.25 && t < 2.5) || (t > 3.5 && t <= 3.75))       //judge good
+                    {
+                        force = 5;
+                    }
+                    else if ((t >= 0.5 && t < 0.75) || (t > 1.25 && t <= 1.5) || (t >= 2.5 && t < 2.75) || (t > 3.25 && t <= 3.5))       //judge great
+                    {
+                        force = 8;
+                    }
+                    else        //judge perfect
+                    {
+                        force = 12;
+                    }
+                    count++;
+                    keyCount++;
+                    Debug.Log(returnForce());
                 }
-                else if ((t >= 0.25 && t < 0.5) || (t > 1.5 && t <= 1.75) || (t >= 2.25 && t < 2.5) || (t > 3.5 && t <= 3.75))       //judge good
-                {
-                    force = 5;
-                }
-                else if ((t >= 0.5 && t < 0.75) || (t > 1.25 && t <= 1.5) || (t >= 2.5 && t < 2.75) || (t > 3.25 && t <= 3.5))       //judge great
-                {
-                    force = 8;
-                }
-                else        //judge perfect
-                {
-                    force = 12;
-                }
-                count++;
-                Debug.Log(returnForce());
             }
         }
-
     }
 }
