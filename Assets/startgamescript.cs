eb5c2dac0e9bbe88269c830c13744905b28cb344
cympyc1785 <cympyc1785@gmail.com>
2021-02-18 01:01:08 +0900

    Revise StartMenu.

--- Assets/startgamescript.cs
+++ Assets/startgamescript.cs
@@ -10,6 +10,9 @@ public class startgamescript : MonoBehaviour
     public Button button2;
     public Button button3;
 
+    public GameObject buttons;
+    public GameObject optionScreen;
+
     public void startgame()
     {
         Debug.Log("start!");
@@ -18,9 +21,20 @@ public class startgamescript : MonoBehaviour
     public void gotooption()
     {
         Debug.Log("option");
+        buttons.SetActive(false);
+        optionScreen.SetActive(true);
     }
     public void exitbutton()
     {
         Debug.Log("Exit!");
+        Application.Quit();
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    }
+    public void closeOption()
+    {
+        buttons.SetActive(true);
+        optionScreen.SetActive(false);
     }
 }
