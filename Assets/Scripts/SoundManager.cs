3552882aa0de0c60d50960d3ff0decb1ba81464b
cympyc1785 <cympyc1785@gmail.com>
2021-02-18 20:50:45 +0900

    Modify SoundManager Script.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,39 +12,61 @@ public class SoundManager : MonoBehaviour
     public GameObject bgmVolumeSlider;
 
     [HideInInspector]
-    public float seVolume;
+    public float seVolume = 0.5f;
     [HideInInspector]
-    public float bgmVolume;
+    public float bgmVolume = 0.5f;
 
-    [HideInInspector]
-    public AudioSource bgmPlayer { get; private set; }
-    [HideInInspector]
-    public AudioSource sePlayer1 { get; private set; }
-    [HideInInspector]
-    public AudioSource sePlayer2 { get; private set; }
+    private AudioSource bgmPlayer;
+    private AudioSource sePlayer;
 
-    public AudioClip bgmClip1;
-    public AudioClip bgmClip2;
+    [SerializeField] AudioClip[] bgmClip;
+    [SerializeField] AudioClip[] SEClip;
+    [SerializeField] AudioClip buttonSoundClip;
 
+    private Dictionary<string, AudioClip> audioClipDic;
 
     void Awake()
     {
-        if (SoundManager.instance == null)
+        if (instance == null)
         {
             instance = this;
         }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
 
         DontDestroyOnLoad(gameObject);
-        seVolume = 0.5f;
-        bgmVolume = 0.5f;
-        bgmPlayer = transform.GetChild(0).GetComponent<AudioSource>();
-        sePlayer1 = transform.GetChild(1).GetComponent<AudioSource>();
-        sePlayer2 = transform.GetChild(2).GetComponent<AudioSource>();
-        bgmPlayer.volume = bgmVolume;
-        sePlayer1.volume = seVolume;
-        sePlayer2.volume = seVolume;
+
+        SetupBGMPlayer();
+        SetupSEPlayer();
+
+        audioClipDic = new Dictionary<string, AudioClip>();
+        audioClipDic.Add("buttonSoundClip", buttonSoundClip);
+        foreach(AudioClip clip in SEClip)
+        {
+            audioClipDic.Add(clip.name, clip);
+        }
     }
 
+    private void SetupBGMPlayer()
+    {
+        bgmPlayer = transform.GetChild(0).GetComponent<AudioSource>();
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.loop = true;
+    }
+
+    private void SetupSEPlayer()
+    {
+        sePlayer = transform.GetChild(1).GetComponent<AudioSource>();
+        sePlayer.volume = seVolume;
+        bgmPlayer.loop = false;
+    }
+
+    void Start()
+    {
+        
+    }
     
     void Update()
     {
@@ -54,33 +76,101 @@ public class SoundManager : MonoBehaviour
             {
                 seVolume = seVolumeSlider.GetComponent<Slider>().value;
                 bgmVolume = bgmVolumeSlider.GetComponent<Slider>().value;
-                bgmPlayer.volume = bgmVolume;
-                sePlayer1.volume = seVolume;
-                sePlayer2.volume = seVolume;
+                SetupBGMPlayer();
+                SetupSEPlayer();
             }
         }
+    }
 
-        else if (SceneManager.GetActiveScene().name == "SelectMenu")
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "StartMenu")
         {
 
         }
-
-        else if (SceneManager.GetActiveScene().name == "SelectExtra")
+        else if (scene.name == "SelectMenu")
         {
 
         }
+        else if (scene.name == "SelectExtra")
+        {
 
-        else if (SceneManager.GetActiveScene().name == "InGame")
+        }
+        else if (scene.name == "InGame")
         {
 
         }
     }
 
-    public void playSound(AudioSource audioPlayer, AudioClip clip)
+    void OnEnable()
     {
-        audioPlayer.Stop();
-        audioPlayer.clip = clip;
-        audioPlayer.time = 0;
-        audioPlayer.Play();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void playSoundClip(AudioClip clip, float volumeRatio = 1f)
+    {
+        sePlayer.PlayOneShot(clip, sePlayer.volume * volumeRatio);
+    }
+
+    public void playSoundDic(string clipName, float volumeRatio = 1f)
+    {
+        if (audioClipDic.ContainsKey(clipName))
+        {
+            sePlayer.PlayOneShot(audioClipDic[clipName], sePlayer.volume * volumeRatio);
+        }
+        else
+        {
+            UnityEngine.Debug.Log(clipName + " 는 저장된 오디오클립에 존재하지 않습니다.");
+        }
+    }
+
+    public void playSoundIdx(int clipIdx, float volumeRatio = 1f)
+    {
+        if (0 < clipIdx && clipIdx < SEClip.Length)
+        {
+            sePlayer.PlayOneShot(SEClip[clipIdx], sePlayer.volume * volumeRatio);
+        }
+        else
+        {
+            UnityEngine.Debug.Log(clipIdx + "번 오디오클립은 존재하지 않습니다.");
+        }
+    }
+
+    public void playRandomSoundDic(string[] clipNameArray, float volumeRatio = 1f)
+    {
+        string clipName = clipNameArray[Random.Range(0, clipNameArray.Length)];
+
+        if (audioClipDic.ContainsKey(clipName))
+        {
+            sePlayer.PlayOneShot(audioClipDic[clipName], sePlayer.volume * volumeRatio);
+        }
+        else
+        {
+            UnityEngine.Debug.Log(clipName + " 는 저장된 오디오클립에 존재하지 않습니다.");
+        }
+    }
+
+    public void playRandomSoundIdx(int[] clipIdxArray, float volumeRatio = 1f)
+    {
+        int clipIdx = clipIdxArray[Random.Range(0, clipIdxArray.Length)];
+
+        if (0 < clipIdx && clipIdx < SEClip.Length)
+        {
+            sePlayer.PlayOneShot(SEClip[clipIdx], sePlayer.volume * volumeRatio);
+        }
+        else
+        {
+            UnityEngine.Debug.Log(clipIdx + "번 오디오클립은 존재하지 않습니다.");
+        }
+    }
+
+    public void playButtonSound()
+    {
+        playSoundClip(buttonSoundClip);
     }
 }
