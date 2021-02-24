0ed00895b116056a8b256b81bcc280a57ac9ae1b
cympyc1785 <cympyc1785@gmail.com>
2021-02-24 19:14:20 +0900

    Modify Extra4 Script.

--- Assets/ScriptsTemp/Extra/Extra4.cs
+++ Assets/ScriptsTemp/Extra/Extra4.cs
@@ -6,7 +6,6 @@ public class Extra4 : MonoBehaviour
 {
     float startTime;
     public GameObject blind;
-    bool freeze;
     float t;
 
     // Start is called before the first frame update
@@ -15,27 +14,26 @@ public class Extra4 : MonoBehaviour
         blind = GameObject.Find("InGameUIObject").GetComponent<InGameUIManager>().blindImage;
         startTime = Time.time;
         blind.SetActive(false);
-        freeze = GetComponent<Character>().freeze;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (freeze)
+        if (GetComponent<Character>().freeze)
         {
             startTime = Time.time - t;
         }
         else
         {
             t = Time.time - startTime;
-            if (t >= 20)
+            if (t >= 8)
             {
                 startTime = Time.time;
                 t = 0;
                 blind.SetActive(false);
             }
 
-            if (t > 10 && t < 20)
+            if (t > 5 && t < 8)
             {
                 blind.SetActive(true);
             }
